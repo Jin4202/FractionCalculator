19e98163c9ace0d66c3fb20fc19fd385d4ee6ee9
Jinseok Heo <genie.h1@gmail.com>
2022-03-03 22:05:22 +0900

    New constant calculation system (2/n) *bug fix

--- app/src/main/java/com/calculator/fractioncalculator/calculation/Lit.java
+++ app/src/main/java/com/calculator/fractioncalculator/calculation/Lit.java
@@ -1,5 +1,10 @@
 package com.calculator.fractioncalculator.calculation;
 
+import android.os.Build;
+
+import androidx.annotation.RequiresApi;
+
+import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Lit {
@@ -17,6 +22,43 @@ public class Lit {
     public Lit(HashMap<Integer, HashMap<Integer, Integer>> n) {
         coefficients = n;
     }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public Lit times(Lit n2) {
+        ArrayList<Lit> lits = new ArrayList<>();
+        for(int n2PiKey : n2.getCoefficients().keySet()) {
+            for (int n2EKey : n2.getCoefficients().getOrDefault(n2PiKey, new HashMap<>()).keySet()) {
+                HashMap<Integer, HashMap<Integer, Integer>> m = new HashMap<>();
+                for(int n1PiKey : this.getCoefficients().keySet()) {
+                    for(int n1EKey : this.getCoefficients().get(n1PiKey).keySet()) {
+                        int c1 = this.getCoefficients().get(n1PiKey).get(n1EKey);
+                        int c2 = n2.getCoefficients().get(n2PiKey).get(n2EKey);
+                        m.getOrDefault(n1PiKey+n2PiKey, new HashMap<>()).put(n1EKey+n2EKey, c1*c2);
+                    }
+                }
+                lits.add(new Lit(m));
+            }
+        }
+        Lit output = new Lit();
+        for(Lit l : lits) {
+            output = add(l);
+        }
+        return output;
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.N)
+    public Lit add(Lit n2) {
+        Lit output = new Lit();
+        for(int piKey : n2.getCoefficients().keySet()) {
+            for (int eKey : n2.getCoefficients().get(piKey).keySet()) {
+                int c1 = this.getCoefficients().getOrDefault(piKey, new HashMap<>()).getOrDefault(eKey, 0);
+                int c2 = n2.getCoefficients().get(piKey).get(eKey);
+                output.getCoefficients().getOrDefault(piKey, new HashMap<>()).put(eKey, c1 + c2);
+            }
+        }
+        return output;
+    }
+
 
     public HashMap<Integer, HashMap<Integer, Integer>> getCoefficients() {
         return coefficients;
--- app/src/main/java/com/calculator/fractioncalculator/calculation/Literal.java
+++ app/src/main/java/com/calculator/fractioncalculator/calculation/Literal.java
@@ -70,42 +70,6 @@ public class Literal implements Element, Calculatetable {
         return i;
     }
 
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public Lit times(Lit n1, Lit n2) {
-        ArrayList<Lit> lits = new ArrayList<>();
-        for(int n2PiKey : n2.getCoefficients().keySet()) {
-            for (int n2EKey : n2.getCoefficients().getOrDefault(n2PiKey, new HashMap<>()).keySet()) {
-                HashMap<Integer, HashMap<Integer, Integer>> m = new HashMap<>();
-                for(int n1PiKey : n1.getCoefficients().keySet()) {
-                    for(int n1EKey : n1.getCoefficients().get(n1PiKey).keySet()) {
-                        int c1 = n1.getCoefficients().get(n1PiKey).get(n1EKey);
-                        int c2 = n2.getCoefficients().get(n2PiKey).get(n2EKey);
-                        m.getOrDefault(n1PiKey+n2PiKey, new HashMap<>()).put(n1EKey+n2EKey, c1*c2);
-                    }
-                }
-                lits.add(new Lit(m));
-            }
-        }
-        Lit output = new Lit();
-        for(Lit l : lits) {
-            output = add(output, l);
-        }
-        return output;
-    }
-
-    @RequiresApi(api = Build.VERSION_CODES.N)
-    public Lit add(Lit n1, Lit n2) {
-        Lit output = new Lit();
-        for(int piKey : n2.getCoefficients().keySet()) {
-            for (int eKey : n2.getCoefficients().get(piKey).keySet()) {
-                int c1 = n1.getCoefficients().getOrDefault(piKey, new HashMap<>()).getOrDefault(eKey, 0);
-                int c2 = n2.getCoefficients().get(piKey).get(eKey);
-                output.getCoefficients().getOrDefault(piKey, new HashMap<>()).put(eKey, c1 + c2);
-            }
-        }
-        return output;
-    }
-
     public Lit getNumerator() {
         return numerator;
     }
